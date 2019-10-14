21885cdc971a9e884a76b743ddf9a63db8f028cb
Max <49519905+Roixxx@users.noreply.github.com>
2019-10-14 15:45:50 +0500

    updated the modal function

--- js/app.js
+++ js/app.js
@@ -27,30 +27,56 @@ $(function() {
     const modalClose = $("[data-close]");
 
     modalCall.on("click", function(event) {
-
         event.preventDefault();
 
         let $this = $(this);
-        let modalId = $this.data("modal");
-        
-        $(modalId).addClass("active");
-        $("body").addClass("scroll-off");
+        let modalId = $this.data('modal');
+
+        $(modalId).addClass('active');
+        $("body").addClass('no-scroll');
+
+        setTimeout(function() {
+            $(modalId).find(".modal__dialog").css({
+                transform: "scale(1)"
+            });
+        }, 200);
+
     });
 
 
     modalClose.on("click", function(event) {
-
         event.preventDefault();
 
         let $this = $(this);
-        let modalParent = $this.parents(".modal");
-        
-        modalParent.removeClass("active");
-        $("body").removeClass("scroll-off");
-        $("body").addClass("scroll-on");
+        let modalParent = $this.parents('.modal');
 
+        modalParent.find(".modal__dialog").css({
+            transform: "scale(0)"
+        });
+
+        setTimeout(function() {
+            modalParent.removeClass('active');
+            $("body").removeClass('no-scroll');
+        }, 200);
     });
 
+
+    $(".modal").on("click", function(event) {
+        let $this = $(this);
+
+        $this.find(".modal__dialog").css({
+            transform: "scale(0)"
+        });
+
+        setTimeout(function() {
+            $this.removeClass('active');
+            $("body").removeClass('no-scroll');
+        }, 200);
+    });
+
+    $(".modal__dialog").on("click", function(event) {
+        event.stopPropagation();
+    });
     
     
 });
